2d1811731944d1a268cfcfadb0dafaf93430fff6
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-07-15 23:58:02 -0700

    Set up firebase.js config for firebase

---
+++ library/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJzK-NmsrajsO1Z0zGpqXEc8CU9Nr_EhQ",
+  authDomain: "nxt-fire-app.firebaseapp.com",
+  projectId: "nxt-fire-app",
+  storageBucket: "nxt-fire-app.appspot.com",
+  messagingSenderId: "1074200402448",
+  appId: "1:1074200402448:web:a31b7b70f41538d2244895",
+  measurementId: "G-SY1PWQSHPR",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
