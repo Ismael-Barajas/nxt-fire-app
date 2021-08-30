81d2d1eb14eabe35646666367e384af67d1fa95a
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-08-30 00:38:53 -0700

    hooks library updated

--- library/hooks.js
+++ library/hooks.js
@@ -1,14 +1,16 @@
 import { auth, firestore } from "../library/firebase";
-
 import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+// Custom hook to read  auth record and user profile doc
 export function useUserData() {
   const [user] = useAuthState(auth);
   const [username, setUsername] = useState(null);
 
   useEffect(() => {
+    // turn off realtime subscription
     let unsubscribe;
+
     if (user) {
       const ref = firestore.collection("users").doc(user.uid);
       unsubscribe = ref.onSnapshot((doc) => {
@@ -17,6 +19,7 @@ export function useUserData() {
     } else {
       setUsername(null);
     }
+
     return unsubscribe;
   }, [user]);
 
