293c1d3f0081169ebcbb9fd1060a85ae8e0cca14
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-08-30 00:40:29 -0700

    username pages completed

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,48 @@
-export default function Profile({}) {
+import { getUserWithUsername, postToJSON } from "../../library/firebase";
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+import Metatags from "../../components/Metatags";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <h1>User Profile</h1>
+      <Metatags
+        title={user.username}
+        description={`${user.username}'s public profile`}
+      />
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
 }
